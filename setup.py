56b20a8ac4a778d13321a16b489708519a5a6d8f
Nitz <nitz.raz@gmail.com>
2015-11-02 14:39:43 +0200

    Relying on pyYaml for yaml

--- setup.py
+++ setup.py
@@ -80,7 +80,9 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[], #TODO complete
+    install_requires=[
+            'pyyaml'
+            ], #TODO complete
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
