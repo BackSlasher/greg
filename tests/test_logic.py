c585c2b4ca052bd266af413c4ca40d80cb682b0d
Nitz <nitz.raz@gmail.com>
2015-11-04 17:07:58 +0200

    Added tests for greg.logic

---
+++ tests/test_logic.py
@@ -0,0 +1,56 @@
+# testing greg.logic
+
+import unittest
+import mock
+import greg.logic
+
+import greg.bridge_provider
+import greg.bridge_builder
+
+class TestLogic(unittest.TestCase):
+    # Test list:
+    # build - failed merge
+    # build - passed merge
+    @mock.patch('greg.bridge_provider')
+    @mock.patch('greg.bridge_builder')
+    def test_test(self,bridge_builder_mock,bridge_provider_mock):
+        probri = bridge_provider_mock.locate_bridge_by_url.return_value
+        bubri = bridge_builder_mock.locate_bridge.return_value
+        bubri.parse_payload.return_value = {
+                'source': {
+                    'provider': 'mockin',
+                    'organization': 'bla',
+                    'name': 'blu',
+                    'commit': 'bli',
+                    },
+                'report': True,
+                'context': 'test',
+                'url': 'glog',
+                'good': False,
+                }
+        greg.logic.build('jenkins','bod','param')
+        probri.post_pr_message.assert_not_called()
+        probri.post_commit_test.assert_called_once_with('bla', 'blu', 'bli',
+                builder_type='jenkins', url='glog', result=False)
+
+    @mock.patch('greg.bridge_provider')
+    @mock.patch('greg.bridge_builder')
+    def test_provider_failed(self,bridge_builder_mock,bridge_provider_mock):
+        probri = bridge_provider_mock.locate_bridge_by_url.return_value
+        bubri = bridge_builder_mock.locate_bridge.return_value
+        bubri.parse_payload.return_value = {
+                'source': {
+                    'provider': 'mockin',
+                    'organization': 'bla',
+                    'name': 'blu',
+                    'commit': 'bli',
+                    },
+                'pr':5,
+                'report': True,
+                'context': 'merge',
+                'url': 'glog',
+                'good': False,
+                }
+        greg.logic.build('jenkins','bod','param')
+        probri.post_pr_message.assert_called_once_with('bla', 'blu', 5, 'Merge **failed**  \nglog')
+        probri.post_commit_test.assert_not_called()
