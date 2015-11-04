8953154c307d53627b2bee4acec3819f4255eeae
Nitz <nitz.raz@gmail.com>
2015-11-04 17:08:45 +0200

    Completed logic tests (for now)

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -54,3 +54,25 @@ class TestLogic(unittest.TestCase):
         greg.logic.build('jenkins','bod','param')
         probri.post_pr_message.assert_called_once_with('bla', 'blu', 5, 'Merge **failed**  \nglog')
         probri.post_commit_test.assert_not_called()
+
+    @mock.patch('greg.bridge_provider')
+    @mock.patch('greg.bridge_builder')
+    def test_provider_failed(self,bridge_builder_mock,bridge_provider_mock):
+        probri = bridge_provider_mock.locate_bridge_by_url.return_value
+        bubri = bridge_builder_mock.locate_bridge.return_value
+        bubri.parse_payload.return_value = {
+                'source': {
+                    'provider': 'mockin',
+                    'organization': 'bla',
+                    'name': 'blu',
+                    'commit': 'bli',
+                    },
+                'pr':5,
+                'report': True,
+                'context': 'merge',
+                'url': 'glog',
+                'good': True,
+                }
+        greg.logic.build('jenkins','bod','param')
+        probri.post_pr_message.assert_not_called()
+        probri.post_commit_test.assert_not_called()
