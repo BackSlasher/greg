ed04c1e24ad1e4f1e9fb17bdf552582340c98731
Nitz <nitz.raz@gmail.com>
2015-12-27 12:48:16 +0200

    Moved all logic to application.py
    WORKS
    now I'll try to migrate it back to greg.server

--- application.py
+++ application.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python
-import greg.server
+import greg.logic
+from flask import Flask, request
 
-applciation = greg.server.app
+application= Flask('application')
+
+# Called by the repository (github / bitbucket) whenever there's a commit / comment
+@application.route('/repo')
+def repo():
+  headers = {k:request.headers[k] for k in request.headers.keys()}
+  querystring = request.args
+  payload = request.data
+  greg.logic.repo(querystring['provider'],payload,headers,querystring)
+
+# Called by the build server when it finished a job (and requires Greg's help in notifying)
+@application.route('/build')
+def build():
+  headers = {k:request.headers[k] for k in request.headers.keys()}
+  querystring = request.args
+  payload = request.data
+  greg.logic.build(querystring['builder'],payload,headers,querystring)
+
+@application.route('/')
+def root():
+  return 'I am Greg'
+
+def run():
+  global application
+  application.run()
+
+if __name__ == '__main__':
+    run()
