9f493aa4c7a86e6b35860735a447b79fe9a20896
Nitz <nitz.raz@gmail.com>
2015-12-27 12:55:45 +0200

    Succesfully migrated to greg.server

--- application.py
+++ application.py
@@ -1,32 +1,7 @@
 #!/usr/bin/env python
-import greg.logic
-from flask import Flask, request
+import greg.server
 
-application= Flask('application')
-
-# Called by the repository (github / bitbucket) whenever there's a commit / comment
-@application.route('/repo')
-def repo():
-  headers = {k:request.headers[k] for k in request.headers.keys()}
-  querystring = request.args
-  payload = request.data
-  greg.logic.repo(querystring['provider'],payload,headers,querystring)
-
-# Called by the build server when it finished a job (and requires Greg's help in notifying)
-@application.route('/build')
-def build():
-  headers = {k:request.headers[k] for k in request.headers.keys()}
-  querystring = request.args
-  payload = request.data
-  greg.logic.build(querystring['builder'],payload,headers,querystring)
-
-@application.route('/')
-def root():
-  return 'I am Greg'
-
-def run():
-  global application
-  application.run()
+application = greg.server.app
 
 if __name__ == '__main__':
-    run()
+    application.run()
