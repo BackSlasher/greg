b20490eac253f57834f5d4357254ad754f448d54
Nitz <nitz.raz@gmail.com>
2016-02-14 17:41:58 +0200

    Added some comments for provider interface

--- greg/provider/__init__.py
+++ greg/provider/__init__.py
@@ -4,14 +4,42 @@ import greg.config
 class BridgeProvider:
   def __init__(self, dic):
     pass
+  # Document type:
+  '''
+repo:
+  provider: X
+  orgranization: X
+  name: X
+event:
+  type: X
+  pr:
+    id: X
+    src_branch: X
+    dst_branch: X
+    same_repo: X # Are the source repo and dest repo of the PR identical
+    reviewers: []
+    approvers: []
+    code_ok: X # Did the code pass validation
+  text: X
+  --OR--
+  changes: []
+  '''
+  # Should also test querystring['token']
   def parse_payload(self, body, headers={}, querystring={}):
     pass
+  # Post the result on a commit.
+  # commit is the commit sha
+  # builder_type is a string used to identify the reporting builder
+  # url is a URL for the builder's result page
+  # result is a boolean indicating pass/fail (Allow None for "in progress")?
   def post_commit_test(self, organization, name, commit, builder_type, url, result):
     pass
   def post_pr_message(self, organization, name, pr, message):
     pass
+  # Make sure a repo has a greg webhook
   def ensure_webhook(self,organization,name,my_url):
       pass
+  # Get all repos in a specific organization
   def list_repos(self,organization):
     pass
 
